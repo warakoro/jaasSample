d64630e286ab2d487fbeb220c732adf999c26329
Mamadou <Mamadou@172.17.3.103>
2015-02-06 18:34:58 -0800

    Read README.md

--- src/java/com/embrella/statup/StartSingleton.java
+++ src/java/com/embrella/statup/StartSingleton.java
@@ -32,10 +32,9 @@ public class StartSingleton {
          mUser = new Goaluser();
 
         mUser.setUsername("mamadou");
-        mUser.setPassword("8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918");
+        mUser.setPassword("8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918"); // this is  admin hashed in SHA-256
         mUser.setUrole("USER_ROLE");
-        this.GUFacade.create(mUser);
-     
+        this.GUFacade.create(mUser);    
         
     }
    
